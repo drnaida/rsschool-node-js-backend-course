92427aafe76180029bd937fcf84c73490a953ddd
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-03 19:22:28 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
+import fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    const path_to_file_to_be_renamed = 'src/fs/files/properFilename.md';
+    fs.access(path_to_file_to_be_renamed, fs.constants.R_OK, (err) => {
+        console.log('\n> Checking whether the folder exists');
+        if (err) {
+            fs.rename('src/fs/files/wrongFilename.txt', path_to_file_to_be_renamed, (error) => {
+                if (error) {
+                    throw new Error('FS operation failed')
+                } else {
+                    console.log('File renamed');
+                }
+            }); 
+        }
+        else {
+            console.error('File already exists');
+        }
+    });
 };
+
+await rename();
