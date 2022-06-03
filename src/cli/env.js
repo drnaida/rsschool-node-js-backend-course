1e4f4a658bc1ef9faa8fa9c40b404592f2a2342f
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-03 22:24:42 +0300

    Feat: implement function that prints custom arguments

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
-    // Write your code here 
+    let output = '';
+    for (let i in process.env) {
+        if (i.startsWith('RSS_')) {
+            output = output + i + '=' + process.env[i] + '; ';
+        }
+    }
+    console.log(output)
 };
+
+await parseEnv();
