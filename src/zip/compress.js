8b5e1dbcb164918bb7d836fc81f51aa5ce9b8bc3
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-05 01:33:47 +0300

    feat: implement function that archives and dearchives

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,10 @@
+import fs, { write } from 'fs';
+import zlib from 'zlib';
 export const compress = async () => {
-    // Write your code here 
+    const content = fs.createReadStream('./src/zip/files/fileToCompress.txt', 'utf8');
+    const gzip = zlib.createGzip();
+    const writeToFile = fs.createWriteStream('./src/zip/files/archive.gz');
+    content.pipe(gzip).pipe(writeToFile);
 };
+
+await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,10 @@
+import zlib from 'zlib';
+import fs from 'fs';
 export const decompress = async () => {
-    // Write your code here 
+    const unzipper = zlib.createUnzip();
+    const fileToUnzip = fs.createReadStream('./src/zip/files/archive.gz');
+    const writeTheContent = fs.createWriteStream('./src/zip/files/fileToCompress.txt');
+    fileToUnzip.pipe(unzipper).pipe(writeTheContent);
 };
+
+await decompress();
