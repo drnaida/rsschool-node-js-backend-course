ba3d6a52e6edfc5960a4b57ee0a9bb6e94ff86b3
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-04 14:46:44 +0300

    feat: create function that prints hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,16 @@
+import crypto from 'crypto';
+import fs from 'fs';
 export const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile('src/hash/files/fileToCalculateHashFor.txt', 'utf8', (err, data) => {
+        if (err) {
+            throw new Error('Hash operation failes');
+        } else {
+            const hashSum = crypto.createHash('sha256');
+            hashSum.update(data);
+            const hex = hashSum.digest('hex');
+            console.log(hex)
+        }
+    });
 };
+
+await calculateHash();
