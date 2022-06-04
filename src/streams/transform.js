10b9cce37f037332d3216346c5e3f0a49f158973
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-05 00:43:28 +0300

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import {Transform, pipeline} from 'stream';
 export const transform = async () => {
-    // Write your code here 
+    const transformed = new Transform({
+        transform(chunk, encryption, callback) {
+            const chunkStringified = chunk.toString();
+            const reversed = chunkStringified.split('').reverse().join('');
+            this.push(reversed + '\n');
+            callback();
+        }
+    });
+
+    pipeline(
+        process.stdin,
+        transformed,
+        process.stdout,
+        err => {
+            console.log('error ${err}')
+        }
+    )
 };
+
+await transform();
