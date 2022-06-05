f1e40352a10a4a09ed4dbc11d1e423b82b4ddbbe
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-05 14:43:07 +0300

    fix: make results appear in the order workers were created in

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,30 +1,29 @@
 import {Worker} from 'worker_threads';
 import {cpus} from 'os';
 export const performCalculations = async () => {
+    const promises = [];
     const cpusAmount = cpus().length;
     let results = [];
     for (let i = 0; i < cpusAmount; i++) {
-        let num = 10 + i;
-        const worker = new Worker("./src/wt/worker.js", {workerData: {num: num}});
-        let output = {status: '', data: ''};
-        worker.once("message", result => {
-            output.data = result;
-        });
-
-        worker.on("error", error => {
-            output.status = 'error';
-            output.data = null;
-        });
-
-        worker.on("exit", exitCode => {
-            if (exitCode == 0) {
-                output.status = 'resolved';
-            }
-            results.push(output);
-            console.log(results);
-        })
-        
+        promises.push(new Promise( (resolve, reject) => {
+            let num = 10 + i;
+            const worker = new Worker("./src/wt/worker.js", {workerData: {num: num}});
+            let output = {status: '', data: ''};
+            worker.on("message", (content) => resolve({
+                status: 'resolved',
+                data: content
+            }));
+    
+            worker.on("error", (err) => resolve({
+                status: 'error',
+                data: null
+            }));
+        }));
     }
+    await Promise.all(promises).then((values) => {
+        console.log(values);
+    });
+
     
 };
 
