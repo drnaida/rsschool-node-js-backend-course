a887ed2d0d31109e43715344ee5633f9b8f6bb95
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-11 23:20:00 +0300

    feat: add path to copy and move functions

--- copyFile.js
+++ copyFile.js
@@ -1,8 +1,11 @@
 import fs from 'fs';
+import { getPath } from './isPathAbsolute.js';
 export const copy = async (whatToRead, whereToCopy) => {
-    const fileName = whatToRead.split('\\').slice(-1)[0] ;
-    const whereToWrite = `${whereToCopy}/${fileName}`;
-    const readableStream = fs.createReadStream(whatToRead);
+    const the_path_read = getPath(whatToRead);
+    const the_path_copy = getPath(whereToCopy);
+    const fileName = the_path_read.split('\\').slice(-1)[0] ;
+    const whereToWrite = `${the_path_copy}/${fileName}`;
+    const readableStream = fs.createReadStream(the_path_read);
     const writableStream = fs.createWriteStream(whereToWrite);
 
     readableStream.setEncoding('utf8');
