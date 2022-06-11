cb82998ce69621c800766ce0ee33566db33124f6
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-11 23:11:26 +0300

    fix: add paths to rename function

--- renameFile.js
+++ renameFile.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
-
+import { getPath } from './isPathAbsolute.js';
 export const rename = async (whatToRename, howToRename) => {
+    const the_path = getPath(whatToRename);
     return new Promise ((resolve, reject) => {
         fs.access(howToRename, fs.constants.R_OK, (err) => {
             console.log('\n> Checking whether the folder exists');
             if (err) {
-                fs.rename(whatToRename, howToRename, (error) => {
+                fs.rename(the_path, howToRename, (error) => {
                     if (error) {
                         reject(error);
                     } else {
