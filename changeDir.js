8f4cc60749e44412b1e121f9e085841401b5720d
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-12 14:53:02 +0300

    Fix: you cannot go higher than root directory

--- changeDir.js
+++ changeDir.js
@@ -1,10 +1,11 @@
 import process from 'process';
 import os from 'os';
+import path from 'path';
  
 export const changeDirectory = async (whereToGo) => {
   try {
   // Change the directory
-  const userHomeDir = os.homedir();
+  const userHomeDir = path.parse(process.cwd()).root;
   const currDir = process.cwd();
   if (whereToGo == 'up') {
     if (currDir == userHomeDir) {
