09df39e8f46f9a791a6480f9afa06bc839dce6e4
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-17 18:51:21 +0300

    feat: create a simple server

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,17 @@
 import 'dotenv/config';
 import * as http from 'http';
+import * as products from '../products.json';
 const server = http.createServer((req, res) => {
-
+    res.statusCode = 200;
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(products));
+    res.end();
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(process.env['EXAMPLE']);
-    console.log('Hello Typescript!');
-    let helloWorld: string = 'Hello, World';
-
-    let firstName: string = 'John';
-    let age: number = 30;
-
-    // Tuple
-    type srtingAndNumber = [string, number];
-    let x: srtingAndNumber = ['Hello', 10];
-
-    // Enums
-    enum Continents {
-        North_America,
-        South_America,
-        Africa,
-        Asia,
-        Europe,
-        Antarctica,
-        Australia
-    }
-
-    let region = Continents.Africa;
+    
 })
 
