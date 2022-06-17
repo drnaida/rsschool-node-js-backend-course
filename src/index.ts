c204a4226af6ac1e423bddd9bb6b32c6e1087e22
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-17 15:56:58 +0300

    feat: configor typescript

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,27 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(process.env['EXAMPLE']);
-})
-// console.log('Hello Typescript!');
-// let helloWorld = 'Hello, World';
+    console.log('Hello Typescript!');
+    let helloWorld: string = 'Hello, World';
 
-// helloWorld = 5;
+    let firstName: string = 'John';
+    let age: number = 30;
+
+    // Tuple
+    type srtingAndNumber = [string, number];
+    let x: srtingAndNumber = ['Hello', 10];
+
+    // Enums
+    enum Continents {
+        North_America,
+        South_America,
+        Africa,
+        Asia,
+        Europe,
+        Antarctica,
+        Australia
+    }
+
+    let region = Continents.Africa;
+})
+
