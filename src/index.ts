e3bdcbe1f27a5c23f714d8002347afbad80b8f15
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-19 00:21:53 +0300

    Feat: store port in .env file

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
