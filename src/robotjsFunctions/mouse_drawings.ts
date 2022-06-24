deae579fb3a1572f27bf4055d5b699771cb5e6f7
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-24 23:06:06 +0300

    Feat: draw a circle

--- src/robotjsFunctions/mouse_drawings.ts
+++ src/robotjsFunctions/mouse_drawings.ts
@@ -1,11 +1,51 @@
 import {mouse_up, mouse_down, mouse_right, mouse_left } from './mouse_movements';
+import { mouse_position } from './mouse_position';
 import robot from 'robotjs';
-const draw_circle = (radius) => {
-
+import { convertCompilerOptionsFromJson } from 'typescript';
+const draw_circle = (radius: number) => {
+    robot.mouseToggle("down")
+    robot.setMouseDelay(2);
+    const {currX, currY} = mouse_position();
+    const centerX = currX + radius;
+    console.log(centerX);
+    const centerY = currY;
+    for (let x = currX; x < (currX + (2 * radius) + 1); x++) {
+        //Math.sqrt(radius ** 2 - (x - centerX)**2) + centerY
+        let y;
+        if (radius ** 2 - ((x - centerX)**2) < 0) {
+            console.log('negative');
+            y = -1 * Math.sqrt(Math.abs(radius ** 2 - ((x - centerX)**2))) + centerY;
+        } else {
+            console.log('positive');
+            y = Math.sqrt(radius ** 2 - ((x - centerX)**2)) + centerY;
+        }
+        console.log(x, centerX, y, centerY);
+        robot.moveMouse(x, y);
+    }
+    for (let x = currX + (2 * radius); x > currX - 1; x--) {
+        //Math.sqrt(radius ** 2 - (x - centerX)**2) + centerY
+        let y;
+        if (radius ** 2 - ((x - centerX)**2) < 0) {
+            console.log('negative');
+            y = centerY + Math.sqrt(Math.abs(radius ** 2 - ((x - centerX)**2)));
+        } else {
+            console.log('positive');
+            y = centerY - Math.sqrt(radius ** 2 - ((x - centerX)**2));
+        }
+        let currentPosition = robot.getMousePos();
+        console.log(x, centerX, y, centerY);
+        robot.moveMouse(x, y);
+    }
+    robot.mouseToggle("up");
 }
 
 const draw_rectangle = (width, length) => {
-
+    robot.mouseToggle("down");
+    mouse_right(width);
+    mouse_down(length);
+    mouse_left(width);
+    mouse_up(length);
+    robot.mouseToggle("up");
 }
 
 const draw_square = (length) => {
