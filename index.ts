2f63ceb04ce20a27a92865088d291ece45dba36f
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-24 14:04:59 +0300

    feaT: open websocket

--- index.ts
+++ index.ts
@@ -5,5 +5,16 @@ import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 3000;
 
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
