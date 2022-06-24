43ef85bd9d603e50f7fa916cb36c4886c9ce8345
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-24 14:42:05 +0300

    feat: parse all commands

---
+++ src/robotjsFunctions/mouse_drawings.ts
@@ -0,0 +1,17 @@
+const draw_circle = (radius) => {
+
+}
+
+const draw_rectangle = (width, length) => {
+
+}
+
+const draw_square = (length) => {
+
+}
+
+export {
+    draw_circle,
+    draw_rectangle,
+    draw_square
+}
---
+++ src/robotjsFunctions/mouse_movements.ts
@@ -0,0 +1,20 @@
+const mouse_up = (lengthToMove) => {
+
+}
+const mouse_down = (lengthToMove) => {
+
+}
+const mouse_right = (lengthToMove) => {
+
+}
+const mouse_left = (lengthToMove) => {
+
+}
+
+
+export {
+    mouse_up,
+    mouse_down,
+    mouse_left,
+    mouse_right
+}
