aa960c98cee62d21a42fc2f5a25b5c580ae50d29
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-25 22:49:33 +0300

    Feat: rewrite websocket with streams

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import Jimp from 'jimp';
 import {httpServer} from './src/http_server/index';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
 import {parseInputCommand} from './src/utils';
 import {mouse_position} from './src/robotjsFunctions/mouse_position';
 import { makeScreenshot } from './src/robotjsFunctions/screenshot';
@@ -8,28 +8,31 @@ import { makeScreenshot } from './src/robotjsFunctions/screenshot';
 const HTTP_PORT = 3000;
 
 const wss = new WebSocketServer({ port: 8080 });
-
 wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
+  const wsStream = createWebSocketStream(ws, { encoding: 'utf-8', decodeStrings: false });
+  wsStream.on('data', (data) => {
     console.log('received: %s', data);
     if (data == 'mouse_position') {
       const {currX, currY} = mouse_position();
       console.log(currX, currY);
-      ws.send(`mouse_position ${currX} px,${currY} px`);
+      wsStream.write(`mouse_position ${currX},${currY}`);
     } else if (data == 'prnt_scrn') {
       const jimg = makeScreenshot();
       jimg.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
-        ws.send(`prnt_scrn ${buffer.toString('base64')}`);
+        wsStream.write(`prnt_scrn ${buffer.toString('base64')}`);
       });
   } else {
       const command = data.toString().split(' ')[0];
       parseInputCommand(data);
-      ws.send(command);
+      wsStream.write(command);
     }
-    
   });
 });
-
+process.on('SIGINT', () => {
+  process.stdout.write('Closing websocket...\n');
+  wss.close();
+  process.exit(0);
+});
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
