b8394d591ed69cab3feadb4ca952e33e9d426740
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-06-26 00:02:47 +0300

    refactor: rewrite httpserver with streams

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -5,13 +5,18 @@ import * as http from 'http';
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
+    const content = fs.createReadStream(file_path);
+    let the_content = '';
+    content.on("data", (chunk) => {
+        the_content += chunk;
     });
+    content.on('close', () => {
+        res.writeHead(200);
+        res.end(the_content);
+    })
+    content.on('error', (err) => {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+    })
 });
